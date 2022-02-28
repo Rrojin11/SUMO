0be5121d35222f349a8b1a01309973c8ae4e7faa
Rrojin11 <dbwls3225@naver.com>
2022-02-28 18:12:09 +0900

    hopcountmatrix0228

--- Geojedo/gj_dqnrun.py
+++ Geojedo/gj_dqnrun.py
@@ -15,9 +15,9 @@ from collections import defaultdict
 
 
 from sumolib import checkBinary
-from gj_dqnTrainedAgent import gj_dqnTrainedAgent
+from gj_dqnTrainedAgent import dqnTrainedAgent
 from gj_dqnenv import gj_dqnEnv
-from gj_dqnagent import gj_dqnAgent
+from gj_dqnagent import dqnAgent
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'],'tools')
@@ -65,13 +65,14 @@ def get_edgesinfo(net):
     edgesinfo = [x.find("lane").attrib for x in alledgelists]
     return edgesinfo
 
-def calculate_connections(edgelists, net):
+def calculate_connections(edgeindex, net):
     # calculate dictionary of reachable edges(next edge) for every edge  
     tree = parse(net)
     root = tree.getroot()
     
     dict_connection = defaultdict(list)
-    dict_connection.update((k,[]) for k in edgelists)
+    #edgelists = [x[1] for x in edgeindex]
+    dict_connection.update((k,[]) for k in edgeindex)
 
     for connection in root.iter("connection"):
         curedge = connection.get("from")
@@ -130,10 +131,10 @@ def dqn_run(num_seed, trained,sumoBinary,plotResult, num_episode,net, trip, rand
     env.reset()
   
     if trained :
-        agent = gj_dqnTrainedAgent( num_seed, edgelists, dict_connection, state_size, action_size,num_episode, dirModel)
+        agent = dqnTrainedAgent( num_seed, edgelists, dict_connection, state_size, action_size,num_episode, dirModel)
         print('**** [TrainedAgent {} Route Start] ****'.format(num_episode))
     else:
-        agent = gj_dqnAgent(num_seed, edgelists, dict_connection, state_size, action_size, num_episode, dirModel)
+        agent = dqnAgent(num_seed, edgelists, dict_connection, state_size, action_size, num_episode, dirModel)
     
     start = time.time()
 
@@ -273,14 +274,24 @@ if __name__ == "__main__":
     else: num_episode = 300
   
     edgelists = get_alledges(net) # 395edges for "./Geojedo/Net/geojedo.net.xml" 
-    dict_connection = calculate_connections(edgelists, net)
+    edgeindex=[]
+    for i,v in enumerate (edgelists):
+        edgeindex.append((i,v))
+    dict_connection = calculate_connections(edgeindex, net)
+    len_edge = len(edgelists)
+    adj = [[0]*len_edge for _ in range(len_edge)]
     
+
+
+    #for i,v in dict_connection.items():
+        
    
     dets = generate_lanedetectionfile(net,det) #이미 생성해둠!
     alldets = get_alldets(edgelists)
+    
 
     """1) Run Simulation"""
-    
+    '''
     trained = False
     num_seed = random.randrange(1000)
     while True: #num_episode같아도 num_seed를 달리해서 겹치는 파일 생성 방지함. 
@@ -289,7 +300,7 @@ if __name__ == "__main__":
     dqn_run(num_seed, trained, sumoBinary, plotResult, num_episode, net, trip, randomrou, add, dirResult,dirModel,
     sumocfg, fcdoutput, edgelists,alldets, dict_connection,veh,destination, state_size, action_size)
     
-    '''
+    
     """2) Run with pre-trained model : Load Weights & Route """
     
     trained = True
